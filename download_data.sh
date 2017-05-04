5975034749b818e4e6b8a8c2c6394f5a06d9fc6e
Lanxiao Bai <hermitebai@outlook.com>
2017-05-04 14:55:10 -0500

    update

--- download_data.sh
+++ download_data.sh
@@ -7,3 +7,9 @@ wget $SOURCE/testdata_small.pkl
 wget $SOURCE/traindata_small.pkl
 cd gowalla
 wget $SOURCE/gowalla/visited_spots.txt
+wget $SOURCE/gowalla/spot_category.txt
+wget $SOURCE/gowalla/user_network.txt
+wget $SOURCE/gowalla/visited_spots.txt
+wget $SOURCE/gowalla/Readme.txt
+
+
